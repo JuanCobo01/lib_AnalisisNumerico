28c592341f5941298a0b88f0126c8918e3ad9f31
Juan Cobo <juan.cobo01@uceva.edu.co>
2024-05-08 03:32:06 +0000

    (feat)Se creo el directorio que almacenara a la biblioteca dinamica

---
+++ lib/DefMatrizJni.java
@@ -0,0 +1,13 @@
+package lib_AnalisisNumerico.lib;
+
+ /*
+* En esta clase se definen las funciones que tendra nuestra biblioteca
+*
+*/
+
+ public class DefMatrizJni{
+
+ public native float[] resolverMatriz(float[] matriz,int size);
+
+
+}
